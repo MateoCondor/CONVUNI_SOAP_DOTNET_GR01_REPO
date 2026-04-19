41837baf07309616d74f752315dbd937b066c5ee
Alexander-Guacan <adguacan@espe.edu.ec>
2026-04-19 12:52:19 -0500

    Improve view styles

--- CLIESC_CONVUNI_SOAP_DOTNET_GR01/Views/LoginView.Designer.cs
+++ CLIESC_CONVUNI_SOAP_DOTNET_GR01/Views/LoginView.Designer.cs
@@ -29,92 +29,131 @@
         private void InitializeComponent()
         {
             label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
             UsernameTextBox = new TextBox();
             PasswordTextBox = new TextBox();
             SubmitButton = new Button();
             ErrorTextBox = new Label();
+            groupBox2 = new GroupBox();
+            groupBox3 = new GroupBox();
+            panel1 = new Panel();
+            label2 = new Label();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 12F);
-            label1.Location = new Point(91, 25);
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(102, 27);
             label1.Name = "label1";
             label1.Size = new Size(102, 21);
             label1.TabIndex = 0;
             label1.Text = "Iniciar Sesión";
             // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(68, 81);
-            label2.Name = "label2";
-            label2.Size = new Size(47, 15);
-            label2.TabIndex = 1;
-            label2.Text = "Usuario";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(48, 120);
-            label3.Name = "label3";
-            label3.Size = new Size(67, 15);
-            label3.TabIndex = 2;
-            label3.Text = "Contraseña";
-            // 
             // UsernameTextBox
             // 
-            UsernameTextBox.Location = new Point(121, 76);
+            UsernameTextBox.BorderStyle = BorderStyle.None;
+            UsernameTextBox.Location = new Point(6, 18);
             UsernameTextBox.Name = "UsernameTextBox";
-            UsernameTextBox.Size = new Size(100, 23);
+            UsernameTextBox.Size = new Size(182, 16);
             UsernameTextBox.TabIndex = 3;
             // 
             // PasswordTextBox
             // 
-            PasswordTextBox.Location = new Point(121, 115);
+            PasswordTextBox.BorderStyle = BorderStyle.None;
+            PasswordTextBox.Location = new Point(6, 18);
             PasswordTextBox.Name = "PasswordTextBox";
-            PasswordTextBox.Size = new Size(100, 23);
+            PasswordTextBox.Size = new Size(182, 16);
             PasswordTextBox.TabIndex = 4;
             PasswordTextBox.UseSystemPasswordChar = true;
             // 
             // SubmitButton
             // 
-            SubmitButton.Location = new Point(101, 158);
+            SubmitButton.BackColor = Color.FromArgb(90, 168, 230);
+            SubmitButton.FlatStyle = FlatStyle.Flat;
+            SubmitButton.ForeColor = Color.White;
+            SubmitButton.Location = new Point(55, 172);
             SubmitButton.Name = "SubmitButton";
-            SubmitButton.Size = new Size(75, 23);
+            SubmitButton.Size = new Size(194, 27);
             SubmitButton.TabIndex = 5;
             SubmitButton.Text = "Ingresar";
-            SubmitButton.UseVisualStyleBackColor = true;
+            SubmitButton.UseVisualStyleBackColor = false;
             SubmitButton.Click += SubmitButton_Click;
             // 
             // ErrorTextBox
             // 
             ErrorTextBox.ForeColor = Color.Red;
-            ErrorTextBox.Location = new Point(12, 208);
+            ErrorTextBox.Location = new Point(24, 218);
             ErrorTextBox.Name = "ErrorTextBox";
-            ErrorTextBox.Size = new Size(254, 35);
+            ErrorTextBox.Size = new Size(250, 35);
             ErrorTextBox.TabIndex = 6;
             ErrorTextBox.Text = "Mensaje de error";
             ErrorTextBox.TextAlign = ContentAlignment.TopCenter;
             ErrorTextBox.Visible = false;
             // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(UsernameTextBox);
+            groupBox2.Location = new Point(55, 66);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(194, 40);
+            groupBox2.TabIndex = 7;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Usuario";
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(PasswordTextBox);
+            groupBox3.Location = new Point(55, 112);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(194, 40);
+            groupBox3.TabIndex = 8;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Contraseña";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(groupBox3);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(groupBox2);
+            panel1.Controls.Add(SubmitButton);
+            panel1.Controls.Add(ErrorTextBox);
+            panel1.Location = new Point(114, 85);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(297, 262);
+            panel1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
+            label2.ForeColor = Color.FromArgb(28, 63, 97);
+            label2.Location = new Point(132, 31);
+            label2.Name = "label2";
+            label2.Size = new Size(265, 25);
+            label2.TabIndex = 9;
+            label2.Text = "CONVERSIÓN DE UNIDADES";
+            // 
             // LoginView
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(278, 252);
-            Controls.Add(ErrorTextBox);
-            Controls.Add(SubmitButton);
-            Controls.Add(PasswordTextBox);
-            Controls.Add(UsernameTextBox);
-            Controls.Add(label3);
+            BackColor = Color.FromArgb(237, 247, 255);
+            ClientSize = new Size(514, 409);
             Controls.Add(label2);
-            Controls.Add(label1);
+            Controls.Add(panel1);
             Name = "LoginView";
             Text = "Conversión de Unidades";
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -122,11 +161,13 @@
         #endregion
 
         private Label label1;
-        private Label label2;
-        private Label label3;
         private TextBox UsernameTextBox;
         private TextBox PasswordTextBox;
         private Button SubmitButton;
         private Label ErrorTextBox;
+        private GroupBox groupBox3;
+        private GroupBox groupBox2;
+        private Panel panel1;
+        private Label label2;
     }
 }
--- CLIESC_CONVUNI_SOAP_DOTNET_GR01/Views/UnitConversionView.Designer.cs
+++ CLIESC_CONVUNI_SOAP_DOTNET_GR01/Views/UnitConversionView.Designer.cs
@@ -29,136 +29,99 @@
         private void InitializeComponent()
         {
             label1 = new Label();
-            label2 = new Label();
             UnitConversionTypeSelect = new ComboBox();
             ConversionUnitSourceSelect = new ComboBox();
             ConversionUnitDestinationSelect = new ComboBox();
-            label3 = new Label();
-            label4 = new Label();
-            label5 = new Label();
             ConversionUnitValueTextBox = new TextBox();
             SubmitButton = new Button();
             ConversionUnitResultTextBox = new TextBox();
-            label6 = new Label();
             ErrorTextBox = new Label();
             LogoutButton = new Button();
+            panel1 = new Panel();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 12F);
-            label1.Location = new Point(109, 60);
+            label1.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
+            label1.ForeColor = Color.FromArgb(28, 63, 97);
+            label1.Location = new Point(75, 22);
             label1.Name = "label1";
-            label1.Size = new Size(179, 21);
+            label1.Size = new Size(228, 25);
             label1.TabIndex = 0;
             label1.Text = "Conversión de Unidades";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(60, 106);
-            label2.Name = "label2";
-            label2.Size = new Size(108, 15);
-            label2.TabIndex = 1;
-            label2.Text = "Tipo de conversión";
             // 
             // UnitConversionTypeSelect
             // 
             UnitConversionTypeSelect.DropDownStyle = ComboBoxStyle.DropDownList;
             UnitConversionTypeSelect.FormattingEnabled = true;
-            UnitConversionTypeSelect.Location = new Point(218, 103);
+            UnitConversionTypeSelect.Location = new Point(24, 83);
             UnitConversionTypeSelect.Name = "UnitConversionTypeSelect";
-            UnitConversionTypeSelect.Size = new Size(121, 23);
+            UnitConversionTypeSelect.Size = new Size(140, 23);
             UnitConversionTypeSelect.TabIndex = 2;
             UnitConversionTypeSelect.SelectedValueChanged += ConversionTypeSelect_SelectedValueChanged;
             // 
-            // UnitConversionSourceSelect
+            // ConversionUnitSourceSelect
             // 
             ConversionUnitSourceSelect.DropDownStyle = ComboBoxStyle.DropDownList;
+            ConversionUnitSourceSelect.FlatStyle = FlatStyle.System;
             ConversionUnitSourceSelect.FormattingEnabled = true;
-            ConversionUnitSourceSelect.Location = new Point(60, 167);
-            ConversionUnitSourceSelect.Name = "UnitConversionSourceSelect";
-            ConversionUnitSourceSelect.Size = new Size(121, 23);
+            ConversionUnitSourceSelect.Location = new Point(24, 146);
+            ConversionUnitSourceSelect.Name = "ConversionUnitSourceSelect";
+            ConversionUnitSourceSelect.Size = new Size(140, 23);
             ConversionUnitSourceSelect.TabIndex = 3;
             // 
-            // UnitConversionDestinationSelect
+            // ConversionUnitDestinationSelect
             // 
             ConversionUnitDestinationSelect.DropDownStyle = ComboBoxStyle.DropDownList;
             ConversionUnitDestinationSelect.FormattingEnabled = true;
-            ConversionUnitDestinationSelect.Location = new Point(218, 167);
-            ConversionUnitDestinationSelect.Name = "UnitConversionDestinationSelect";
-            ConversionUnitDestinationSelect.Size = new Size(121, 23);
+            ConversionUnitDestinationSelect.Location = new Point(210, 146);
+            ConversionUnitDestinationSelect.Name = "ConversionUnitDestinationSelect";
+            ConversionUnitDestinationSelect.Size = new Size(140, 23);
             ConversionUnitDestinationSelect.TabIndex = 4;
             // 
-            // label3
+            // ConversionUnitValueTextBox
             // 
-            label3.AutoSize = true;
-            label3.Location = new Point(60, 145);
-            label3.Name = "label3";
-            label3.Size = new Size(43, 15);
-            label3.TabIndex = 5;
-            label3.Text = "Origen";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(218, 145);
-            label4.Name = "label4";
-            label4.Size = new Size(47, 15);
-            label4.TabIndex = 6;
-            label4.Text = "Destino";
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Location = new Point(59, 207);
-            label5.Name = "label5";
-            label5.Size = new Size(33, 15);
-            label5.TabIndex = 7;
-            label5.Text = "Valor";
-            // 
-            // UnitConversionValueTextBox
-            // 
-            ConversionUnitValueTextBox.Location = new Point(60, 236);
-            ConversionUnitValueTextBox.Name = "UnitConversionValueTextBox";
-            ConversionUnitValueTextBox.Size = new Size(121, 23);
+            ConversionUnitValueTextBox.Location = new Point(24, 215);
+            ConversionUnitValueTextBox.Name = "ConversionUnitValueTextBox";
+            ConversionUnitValueTextBox.Size = new Size(140, 23);
             ConversionUnitValueTextBox.TabIndex = 8;
             ConversionUnitValueTextBox.KeyPress += ConversionValueTextBox_KeyPress;
             // 
             // SubmitButton
             // 
-            SubmitButton.Location = new Point(157, 287);
+            SubmitButton.BackColor = Color.FromArgb(90, 168, 230);
+            SubmitButton.FlatStyle = FlatStyle.Flat;
+            SubmitButton.ForeColor = Color.White;
+            SubmitButton.Location = new Point(45, 267);
             SubmitButton.Name = "SubmitButton";
-            SubmitButton.Size = new Size(75, 23);
+            SubmitButton.Size = new Size(99, 31);
             SubmitButton.TabIndex = 9;
             SubmitButton.Text = "Convertir";
-            SubmitButton.UseVisualStyleBackColor = true;
+            SubmitButton.UseVisualStyleBackColor = false;
             SubmitButton.Click += SubmitButton_Click;
             // 
-            // UnitConversionResultTextBox
+            // ConversionUnitResultTextBox
             // 
-            ConversionUnitResultTextBox.Location = new Point(218, 236);
-            ConversionUnitResultTextBox.Name = "UnitConversionResultTextBox";
+            ConversionUnitResultTextBox.BackColor = Color.White;
+            ConversionUnitResultTextBox.Location = new Point(210, 215);
+            ConversionUnitResultTextBox.Name = "ConversionUnitResultTextBox";
             ConversionUnitResultTextBox.ReadOnly = true;
-            ConversionUnitResultTextBox.Size = new Size(121, 23);
+            ConversionUnitResultTextBox.Size = new Size(140, 23);
             ConversionUnitResultTextBox.TabIndex = 10;
-            // 
-            // label6
-            // 
-            label6.AutoSize = true;
-            label6.Location = new Point(218, 207);
-            label6.Name = "label6";
-            label6.Size = new Size(59, 15);
-            label6.TabIndex = 11;
-            label6.Text = "Resultado";
             // 
             // ErrorTextBox
             // 
             ErrorTextBox.ForeColor = Color.Red;
-            ErrorTextBox.Location = new Point(12, 333);
+            ErrorTextBox.Location = new Point(24, 319);
             ErrorTextBox.Name = "ErrorTextBox";
-            ErrorTextBox.Size = new Size(371, 36);
+            ErrorTextBox.Size = new Size(326, 36);
             ErrorTextBox.TabIndex = 12;
             ErrorTextBox.Text = "Mensaje de error";
             ErrorTextBox.TextAlign = ContentAlignment.TopCenter;
@@ -166,55 +129,115 @@
             // 
             // LogoutButton
             // 
-            LogoutButton.Location = new Point(308, 12);
+            LogoutButton.BackColor = Color.FromArgb(90, 168, 230);
+            LogoutButton.FlatStyle = FlatStyle.Flat;
+            LogoutButton.ForeColor = Color.White;
+            LogoutButton.Location = new Point(222, 269);
             LogoutButton.Name = "LogoutButton";
-            LogoutButton.Size = new Size(75, 23);
+            LogoutButton.Size = new Size(107, 29);
             LogoutButton.TabIndex = 13;
             LogoutButton.Text = "Cerrar sesión";
-            LogoutButton.UseVisualStyleBackColor = true;
+            LogoutButton.UseVisualStyleBackColor = false;
             LogoutButton.Click += LogoutButton_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(ConversionUnitResultTextBox);
+            panel1.Controls.Add(ConversionUnitValueTextBox);
+            panel1.Controls.Add(ConversionUnitSourceSelect);
+            panel1.Controls.Add(ConversionUnitDestinationSelect);
+            panel1.Controls.Add(UnitConversionTypeSelect);
+            panel1.Controls.Add(LogoutButton);
+            panel1.Controls.Add(ErrorTextBox);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(SubmitButton);
+            panel1.Location = new Point(25, 34);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(369, 361);
+            panel1.TabIndex = 14;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(24, 65);
+            label2.Name = "label2";
+            label2.Size = new Size(108, 15);
+            label2.TabIndex = 14;
+            label2.Text = "Tipo de conversión";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(24, 128);
+            label3.Name = "label3";
+            label3.Size = new Size(43, 15);
+            label3.TabIndex = 15;
+            label3.Text = "Origen";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(210, 128);
+            label4.Name = "label4";
+            label4.Size = new Size(47, 15);
+            label4.TabIndex = 16;
+            label4.Text = "Destino";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(24, 197);
+            label5.Name = "label5";
+            label5.Size = new Size(33, 15);
+            label5.TabIndex = 17;
+            label5.Text = "Valor";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(210, 197);
+            label6.Name = "label6";
+            label6.Size = new Size(59, 15);
+            label6.TabIndex = 18;
+            label6.Text = "Resultado";
             // 
             // UnitConversionView
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(395, 378);
-            Controls.Add(LogoutButton);
-            Controls.Add(ErrorTextBox);
-            Controls.Add(label6);
-            Controls.Add(ConversionUnitResultTextBox);
-            Controls.Add(SubmitButton);
-            Controls.Add(ConversionUnitValueTextBox);
-            Controls.Add(label5);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(ConversionUnitDestinationSelect);
-            Controls.Add(ConversionUnitSourceSelect);
-            Controls.Add(UnitConversionTypeSelect);
-            Controls.Add(label2);
-            Controls.Add(label1);
+            BackColor = Color.FromArgb(237, 247, 255);
+            ClientSize = new Size(421, 433);
+            Controls.Add(panel1);
             Name = "UnitConversionView";
             Text = "Conversión de Unidades";
             FormClosed += UnitConversionView_FormClosed;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
         private Label label1;
-        private Label label2;
         private ComboBox UnitConversionTypeSelect;
         private ComboBox ConversionUnitSourceSelect;
         private ComboBox ConversionUnitDestinationSelect;
-        private Label label3;
-        private Label label4;
-        private Label label5;
         private TextBox ConversionUnitValueTextBox;
         private Button SubmitButton;
         private TextBox ConversionUnitResultTextBox;
-        private Label label6;
         private Label ErrorTextBox;
         private Button LogoutButton;
+        private Panel panel1;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
     }
 }
